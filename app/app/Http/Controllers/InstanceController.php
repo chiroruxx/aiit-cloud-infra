bb7be7d8b85177bb897301411f016cef7c2476ab
chiroruxx <dormir.kazu@gmail.com>
2021-10-31 21:21:05 +0900

    インスタンス作成APIを作成

--- app/app/Http/Controllers/InstanceController.php
+++ app/app/Http/Controllers/InstanceController.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Jobs\CreateInstanceRequest;
 use App\Models\Instance;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class InstanceController extends Controller
 {
@@ -14,5 +16,12 @@ class InstanceController extends Controller
         $instances = Instance::all();
 
         return response()->json($instances);
+    }
+
+    public function store(): Response
+    {
+        CreateInstanceRequest::dispatch();
+
+        return response('', Response::HTTP_ACCEPTED);
     }
 }
